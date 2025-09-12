ed876747673b91cc621cfca6731efd04f787bbd6
Neeraj Kumar <nkurmbanshi@yahoo.com>
2025-09-13 01:29:10 +0530

    Job_Portal-API allowed in HOST

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -25,8 +25,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['modelviewset-api.onrender.com']
-# 'modelviewset-api.onrender.com'
+ALLOWED_HOSTS = ['job-portal-api-0trm.onrender.com']
 
 # Application definition
 
