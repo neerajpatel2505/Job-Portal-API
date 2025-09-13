b7c562df03044a39426f6e70c880a5ee9a526163
Neeraj Kumar <nkurmbanshi@yahoo.com>
2025-09-13 14:12:22 +0530

    Update static path

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -114,10 +114,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 # This is required for collectstatic
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Optional but recommended
 STATICFILES_DIRS = [
