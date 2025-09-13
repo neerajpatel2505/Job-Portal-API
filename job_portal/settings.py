74ee2854168308bcef1cbcb32c3837c5c79ddbb5
Neeraj Kumar <nkurmbanshi@yahoo.com>
2025-09-13 13:53:51 +0530

    Update allowed host

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -25,7 +25,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['job-portal-api1.onrender.com']
+ALLOWED_HOSTS = ['job-portal-api-1-zrl1.onrender.com']
 
 # Application definition
 
